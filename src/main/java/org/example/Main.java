9fe90772425c4bc56d6cc50234911ad241d7ad70
replakcan <alper.makcan@gmail.com>
2024-11-12 16:02:04 +0300

    Creates isPalindrome method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,23 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        isPalindrome(-1221);
     }
+
+    public static boolean isPalindrome(int num) {
+        int i = 0;
+        String numToString = Integer.toString(num).replaceAll("-", "");
+        int range = numToString.length();
+        while (i < range) {
+            if ( numToString.charAt(i) == numToString.charAt(range - (i + 1))){
+                i++;
+            } else {
+                System.out.println("false");
+                return false;
+            }
+        }
+        System.out.println("true");
+        return true;
+    }
+
 }
