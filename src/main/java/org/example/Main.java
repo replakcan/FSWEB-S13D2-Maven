77e7e4e99b2a9a1dbf18078c7394a6e939bd4d50
replakcan <alper.makcan@gmail.com>
2024-11-12 19:10:06 +0300

    Creates a method called numberToWords
    that returns given number's digits as a String

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,13 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         isPalindrome(-1221);
         isPerfectNumber(28);
+        numberToWords(34501);
     }
 
     public static boolean isPalindrome(int num) {
@@ -34,6 +37,57 @@ public class Main {
         System.out.println("sum:" + sum);
         return sum == num;
     }
+    public static String numberToWords(int num) {
+    /*
+    sayiyi string'e cevir, tum karakterleri gez, karaktere denk gelen yaziyi array'e pushla
+     */
 
+        if (num < 0){
+            return "Invalid Value";
+        }
+        String numToString = Integer.toString(num);
+        System.out.println(num);
+
+        int range = numToString.length();
+
+        String stringVersion = "";
+
+        for (int i = 0; i < range; i++){
+            switch (numToString.toCharArray()[i]){
+                case '0':
+                     stringVersion += "Zero ";
+                    break;
+                case '1':
+                    stringVersion += "One ";
+                    break;
+                case '2':
+                    stringVersion += "Two ";
+                    break;
+                case '3':
+                    stringVersion += "Three ";
+                    break;
+                case '4':
+                    stringVersion += "Four ";
+                    break;
+                case '5':
+                    stringVersion += "Five ";
+                    break;
+                case '6':
+                    stringVersion += "Six ";
+                    break;
+                case '7':
+                    stringVersion += "Seven ";
+                    break;
+                case '8':
+                    stringVersion += "Eight ";
+                    break;
+                case '9':
+                    stringVersion += "Nine ";
+                    break;
+            }
+        }
+        System.out.println(stringVersion.trim());
+        return stringVersion.trim();
+    }
 
 }
